770d7164293dc765d41b4d306c091cd4033a7efb
Rinat <rinat.astr2@gmail.com>
2020-08-04 19:02:15 +0600

    Initial commit

---
+++ src/test/java/tests/GoogleTest.java
@@ -0,0 +1,43 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
+
+//Добавление товара в корзину
+class AlfaBankTest {
+    @Test
+    void selenideSearchTest() {
+        open("https://alfabank.ru/");
+
+        $(byText("Вклады")).click();
+        $("body").shouldHave(text("Депозиты"));
+        $$(byText("Депозиты")).find(visible).click();
+        $(byText("Архивные депозиты")).click();
+
+        $("html").shouldHave(text("Архивные депозиты"));
+    }
+}
+class AlfaBank1Test {
+    @Test
+    void selenideSearchTest() {
+        open("https://alfabank.ru/");
+
+        $(byText("Вклады")).click();
+        $(".selected").sibling(4).click();
+
+        $(".col-sm-8").parent().shouldHave(text("АО «АЛЬФА-БАНК» является участником системы обязательного страхования вкладов. Это означает,\n" +
+                "что средства клиентов Альфа-Банка, размещенные во вкладах и на банковских счетах,\n" +
+                "надежно защищены государством. Если банк прекращает работу при наступлении страхового случая,\n" +
+                "по закону его вкладчикам выплачиваются возмещения по вкладам."));
+    }
+}
+
+
+
+
+
+
+
