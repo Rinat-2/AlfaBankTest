79a6fa4b6bb84610661ac4153120bc9b5805e7b2
Rinat <rinat.astr2@gmail.com>
2020-08-04 19:34:29 +0600

    commit

--- src/test/java/tests/AlfaBank.java
+++ src/test/java/tests/AlfaBank.java
@@ -6,7 +6,7 @@ import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 
-
+//
 class AlfaBankTest {
     @Test
     void selenideSearchTest() {
