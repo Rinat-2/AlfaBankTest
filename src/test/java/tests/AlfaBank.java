2b5f8cf81b67b8cca1afd0b6ae7c79733d16c5f9
Rinat <rinat.astr2@gmail.com>
2020-08-09 22:52:39 +0600

    commit

--- src/test/java/tests/AlfaBank.java
+++ src/test/java/tests/AlfaBank.java
@@ -3,9 +3,9 @@ package tests;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 class AlfaBankContributionTest {
     @Test
@@ -19,5 +19,18 @@ class AlfaBankContributionTest {
                 "что средства клиентов Альфа-Банка, размещенные во вкладах и на банковских счетах,\n" +
                 "надежно защищены государством. Если банк прекращает работу при наступлении страхового случая,\n" +
                 "по закону его вкладчикам выплачиваются возмещения по вкладам."));
+    }
+}
+class AlfaBankDepositTest {
+    @Test
+    void selenideSearchTest() {
+        open("https://alfabank.ru/");
+
+        $(byText("Вклады")).click();
+        $("body").shouldHave(text("Депозиты"));
+        $$(byText("Депозиты")).find(visible).click();
+        $(byText("Архивные депозиты")).click();
+
+        $("html").shouldHave(text("Архивные депозиты"));
     }
 }
